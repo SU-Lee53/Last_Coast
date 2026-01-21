31692aab4587fa29f81a753d0e6dd509d81df43d
SU-Lee53 <dltmddnr05@naver.com>
2026-01-21 20:45:01 +0900

    Engine : NetworkTestScene

---
+++ Game/Client/Client/NetworkTestScene.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "NetworkTestScene.h"
+#include "DebugPlayer.h"
+
+void NetworkTestScene::BuildObjects()
+{
+	m_pPlayer = std::make_shared<DebugPlayer>();
+	//std::shared_ptr<TexturedSprite> pTextureSprite = std::make_shared<TexturedSprite>("Opening", 0.f, 0.f, 1.0f, 1.0f);
+	//m_pSprites.push_back(pTextureSprite);
+
+	Scene::InitializeObjects();
+}
+
+void NetworkTestScene::OnEnterScene()
+{
+}
+
+void NetworkTestScene::OnLeaveScene()
+{
+}
+
+void NetworkTestScene::ProcessInput()
+{
+}
+
+void NetworkTestScene::Update()
+{
+	NETWORK->ConnectToServer();
+}
+
+void NetworkTestScene::Render(ComPtr<ID3D12GraphicsCommandList> pd3dCommansList)
+{
+}
---
+++ Game/Client/Client/NetworkTestScene.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Scene.h"
+
+class NetworkTestScene : public Scene {
+public:
+	void BuildObjects() override;
+	void OnEnterScene() override;
+	void OnLeaveScene() override;
+	void ProcessInput() override;
+	void Update() override;
+	void Render(ComPtr<ID3D12GraphicsCommandList> pd3dCommansList) override;
+};
+
